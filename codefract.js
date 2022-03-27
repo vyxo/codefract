1655ca7fdce6d9316418d3ff1fdb78692d999ac4
Vyxo <100800443+vyxo@users.noreply.github.com>
2022-03-27 19:15:33 +0000

    added basic functions

--- codefract.js
+++ codefract.js
@@ -1,1 +1,85 @@
-
+module.exports = {
+  range: (start, end, step) => {
+    if (!start) {
+      start = 0
+    }
+    if (!end) {
+      end = 1
+    }
+    if (!step) {
+      step = 1
+    }
+    let list = []
+    for (i = start; i < end; i += step) {
+      list.push(i)
+    }
+    return list
+  },
+  copy: (input, formula) => {
+    if (Array.isArray(input)) {
+      const list = []
+      if ((typeof formula[0]) === 'number' && (typeof formula[1]) === 'number') {
+        if (!(typeof formula[2]) === 'number' || !formula[2]) {
+          formula[2] = 1
+        }
+        console.log(formula[2])
+        for (i = formula[0]; i < input.length && i < formula[1]; i += formula[2]) {
+          list.push(input[i])
+        }
+        return list
+      } else {
+        console.log('Invalid copy formulas')
+      }
+    } else {
+      console.log(`Your input: ${input}\nis INVALID`)
+    }
+  },
+  stringer: (length, callback, charset) => {
+    if (!charset || ! (typeof charset) === 'string') {
+      charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    }
+    const list = [];
+    for (i = 0; i < length; i++) {
+      list.push(null)
+    }
+    for (n = 0; n < charset.length ** length; n++) {
+      list.forEach((item, i) => {
+        list[i] = charset[Math.floor((n * charset.length * charset.length ** i) / ((charset.length ** length))) % charset.length]
+      })
+      let string = ''
+      for (i = 0; i < list.length; i++) {
+        string += list[i]
+      }
+      callback(string)
+    }
+  },
+  simplify: (input) => {
+    if (Array.isArray(input)) {
+      const list = []
+      for (item of input) {
+        if (!list.includes(item)) {
+          list.push(item)
+        }
+      }
+      return list
+    } else {
+      console.log('Input must be an Array!')
+    }
+  },
+  frequency: (input) => {
+    if (Array.isArray(input)) {
+      const count = new Map()
+      for (item of input) {
+        let value = count.get(item)
+        if (value) {
+          count.set(item, value + 1)  
+        } else {
+          count.set(item, 1)
+        }
+      }
+      return count
+    } else {
+      console.log('Input must be an Array!')
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const cf = require('./codefract')
 
+// loop through a range of integers
+for (num of cf.range(0, 10, 2)) {
+  console.log(num)
+}
+
+const numbers = cf.range(0, 10, 1)
+const sus = cf.range(0, 42) // defaults to (0, 42, 1)
+
+// copies odd values from 1 to 10 in numbers
+const copy = cf.copy(numbers, [1, 10, 2])
+const stillWorks = cf.copy(numbers, [1, 10])
+console.log(copy, stillWorks)
+
+const websites = ['discord', 'facebook', 'discord', 'reddit', 'github', 'github', 'discord']
+const simplified = cf.simplify(websites)
+const counts = cf.frequency(websites)
+console.log(simplified, counts)
+
+// create all possible strings with length 2 using the default charset <numbers and letters>
+/*
+cf.stringer(2, (string) => {
+  console.log(string)
+})
+*/
+// get all possible hexadecimals
+/*
+cf.stringer(6, (string) => {
+  console.log(string)
+}, '0123456789ABCDEF')
+*/
